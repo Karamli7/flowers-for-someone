78eb196e0f90450450b81257ccc0a3cb84934f9a
Karamli7 <Cinarkrml7@gmail.com>
2025-07-10 01:26:38 +0300

    index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `I Have Something`.split('')
+const text = `Küçük bir hediye`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
